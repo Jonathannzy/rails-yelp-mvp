d0e31ed848d0a0cc12da9f60babf1b28825c3a57
(Jonathan Ng) <nzyjonathan@gmail.com>
2020-05-16 21:50:02 +0800

    update

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,25 @@
 class RestaurantsController < ApplicationController
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(rest_params)
+    Restaurant.save
+    redirect_to restaurants_path
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  private
+  def rest_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,12 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @reviewed_restaurant = Restaurant.find(params[:restaurant_id])
+    @new_review = @reviewed_restaurant.reviews.build
+
+  end
+
+  def create
+  end
+
 end
